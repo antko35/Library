34aa7925d37cf0d32c86516aaeaf113a196b7eef
antko35 <kovsheranton@gmail.com>
2024-10-03 13:01:14 +0300

    руфвук

--- frontend/Library/src/сomponents/header/Header.js
+++ frontend/Library/src/сomponents/header/Header.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 import { Content } from 'antd/es/layout/layout';
 
 const menuItems=[
-        {key:'all-books',label: <Link to={"/"}>Home</Link>},
+        {key:'all-books',label: <Link to={"/"}>Your Books</Link>},
         {key:'my-books',label: <Link to={"/books"}>All Books</Link>},
         {key:'profile',label: <Link to={"/profile"}>Profile</Link>,style:{marginLeft :'auto'}}
     ];
