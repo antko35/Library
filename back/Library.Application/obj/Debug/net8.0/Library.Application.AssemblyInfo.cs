977816e23dacf0a2cc9478f6e72d449c741bf8e6
antko35 <kovsheranton@gmail.com>
2024-10-17 22:41:43 +0300

    beforecommit

--- back/Library.Application/obj/Debug/net8.0/Library.Application.AssemblyInfo.cs
+++ back/Library.Application/obj/Debug/net8.0/Library.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+973633b1035b9dde683807e01f91d9cce95dcb92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d00abc06b83e8bbb0ae85a71dd648748000a0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
